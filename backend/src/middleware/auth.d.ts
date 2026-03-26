18b140c1166024986ad55948ed94e61d1880657b
Joana Freitas <Joanacf1005@gmail.com>
2026-03-26 11:54:57 +0000

    FIX: Deploy issues with lint

--- backend/src/middleware/auth.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response, NextFunction } from 'express';
-export type AuthedRequest = Request & {
-    user?: {
-        id: string;
-    };
-};
-export declare const verifyToken: (req: AuthedRequest, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=auth.d.ts.map
