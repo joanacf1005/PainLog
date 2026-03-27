309a9e1ce39668ae060114fe39eede296d6fac56
Joana Freitas <Joanacf1005@gmail.com>
2026-03-27 11:47:35 +0000

    FEAT: Login and register with auth service

--- frontend/painlog-frontend/src/app/app.routes.ts
+++ frontend/painlog-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Register } from './auth/register/register';
+import { LoginComponent } from './auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: Register },
+  { path: '**', redirectTo: 'login' }
+];
