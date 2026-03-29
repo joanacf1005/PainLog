2016551f7127b1279f082a9f6480fa714943212c
Joana Freitas <Joanacf1005@gmail.com>
2026-03-29 16:27:13 +0100

    FEAT: small adjustmenst to nav-menu

--- frontend/painlog-frontend/src/app/app.routes.ts
+++ frontend/painlog-frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { Register } from './auth/register/register';
-import { LoginComponent } from './auth/login/login';
+import { Login } from './auth/login/login';
 import { HomePage } from './app-pages/home-page/home-page/home-page';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'homepage', component: HomePage },
   { path: '**', redirectTo: 'login' }
