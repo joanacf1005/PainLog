964f81b033b507028dd17bebd80181eb9e300419
Joana Freitas <Joanacf1005@gmail.com>
2026-04-05 10:06:20 +0100

    FEAT: Resources Page and overall CSS adjustments

---
+++ frontend/painlog-frontend/src/app/app-pages/resources-page/resources-page.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ResourcesPage } from './resources-page';
+
+describe('ResourcesPage', () => {
+  let component: ResourcesPage;
+  let fixture: ComponentFixture<ResourcesPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ResourcesPage],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ResourcesPage);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
