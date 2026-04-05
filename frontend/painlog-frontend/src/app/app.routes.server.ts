e1714d4e23acdc74b5b23a5727a8e7fa35665573
Joana Freitas <Joanacf1005@gmail.com>
2026-04-05 18:44:44 +0100

    FEAT: CI Pipeline with lint, build and deploy

--- frontend/painlog-frontend/src/app/app.routes.server.ts
+++ frontend/painlog-frontend/src/app/app.routes.server.ts
@@ -1,8 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Client },
+  { path: 'login', renderMode: RenderMode.Client },
+  { path: 'register', renderMode: RenderMode.Client },
+  { path: 'homepage', renderMode: RenderMode.Server },
+  { path: 'newentry', renderMode: RenderMode.Server },
+  { path: 'edit-entry/:id', renderMode: RenderMode.Server },
+  { path: 'reportspage', renderMode: RenderMode.Server },
+  { path: 'details-page/:id', renderMode: RenderMode.Server },
+  { path: 'statistics', renderMode: RenderMode.Server },
+  { path: 'resourcespage', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Server }
 ];
